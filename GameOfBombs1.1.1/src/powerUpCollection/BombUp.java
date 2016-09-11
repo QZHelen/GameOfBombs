a63781b731ba163e27847ab785fc19160b8734e2
QZHelen <zihe@live.unc.edu>
2016-09-11 16:50:01 -0400

    working on PowerUp

--- GameOfBombs1.1.1/src/powerUpCollection/BombUp.java
+++ GameOfBombs1.1.1/src/powerUpCollection/BombUp.java
@@ -1,5 +1,10 @@
 package powerUpCollection;
 
-public class BombUp {
+public class BombUp extends PowerUp {
+
+	public BombUp(int row, int col, int width, int height) {
+		super(row, col, width, height);
+		
+	}
 
 }
