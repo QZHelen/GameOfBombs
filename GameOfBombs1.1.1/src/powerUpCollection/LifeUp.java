aa5894f94b2a3bff3c2e346966668328b2699586
yunchu <yunchu@live.unc.edu>
2016-09-11 18:25:17 -0400

    powerup type created

--- GameOfBombs1.1.1/src/powerUpCollection/LifeUp.java
+++ GameOfBombs1.1.1/src/powerUpCollection/LifeUp.java
@@ -2,9 +2,9 @@ package powerUpCollection;
 
 import characterCollection.Player;
 
-public class FireUp extends PowerUp {
+public class LifeUp extends PowerUp {
 
-	public FireUp(int row, int col, int width, int height, PowerUpType powerUpType, Player myplayer) {
+	public LifeUp(int row, int col, int width, int height, PowerUpType powerUpType, Player myplayer) {
 		super(row, col, width, height, powerUpType, myplayer);
 		// TODO Auto-generated constructor stub
 	}
